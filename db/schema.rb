b3674d394dd9e0de1fdb0f8aea77ae76a46836a9
shaunwmusic <shaunwmusic@gmail.com>
2018-04-20 18:10:23 -0500

    migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314140600) do
+ActiveRecord::Schema.define(version: 20180420212440) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20180314140600) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "user"
+    t.text "body"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
   end
 
 end
